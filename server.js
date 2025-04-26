8586b73b9ed53c1c8960971d13a8a2d42217db2a
Daontay Knight <daont@MacBook-Pro-2.local>
2025-04-25 22:03:59 -0400

    Cleartext Fix

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 const express = require("express");
-const helmet = require("helmet");
+const helmet  = require("helmet");
+const cors    = require("cors");
+const https   = require("https");
+const fs      = require("fs");
 const echoaRoutes = require("./src/echoa/routes");
 
-const app = express();
-const port = 8003;
+const app  = express();
+const port = process.env.PORT || 8003;
 
 app.use(express.json());
 app.use(helmet());
-
-const cors = require("cors");
-app.use(cors({
-    origin: '*'
-}));
+app.use(cors({ origin: '*' }));
 
 app.get("/", (req, res) => {
-    res.send("Here are the 50 best albums of all time");
-})
-
+  res.send("Here are the 50 best albums of all time");
+});
 app.use("/api/v1/echoa", echoaRoutes);
 
+// Load your cert & key
+const options = {
+  key:  fs.readFileSync("./certs/key.pem"),
+  cert: fs.readFileSync("./certs/cert.pem"),
+};
 
-
-app.listen(port, () => console.log('running on ${port}'));
+https.createServer(options, app)
+     .listen(port, () => {
+       console.log(`HTTPS server running on port ${port}`);
+     });
